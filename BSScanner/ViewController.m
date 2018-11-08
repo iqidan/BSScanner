f77ee8858a5c0274eb62e799d1de7df7cf68455c
iqidan <1506776365@qq.com>
2018-11-08 15:32:02 +0800

    添加注释

--- BSScanner/ViewController.m
+++ BSScanner/ViewController.m
@@ -27,14 +27,17 @@
 - (IBAction)scan:(id)sender {
     BSScannerController *scanVC = [BSScannerController new];
     __weak ViewController *weakSelf = self;
+    //成功
     scanVC.BSScanSuccessBlock = ^(NSString *res){
         NSLog(@"BSScanSuccessBlock res=%@", res);
         [weakSelf.contentTF setText:res];
     };
+    //失败
     scanVC.BSScanFailBlock = ^(NSString *res){
         NSLog(@"BSScanFailBlock res=%@", res);
         [weakSelf.contentTF setText:res];
     };
+    //取消
     scanVC.BSScanCancelBlock = ^(NSString *res){
         NSLog(@"BSScanCancelBlock res=%@", res);
         [weakSelf.contentTF setText:res];
--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 	iOS 二维码、条码扫描
 
 # Use
-# 1)
+## 1)
 	BSScanner/BSScanner/BSScanner 文件拖入工程
-# 2)
+## 2)
 	BSScannerController *scanVC = [BSScannerController new];
     __weak ViewController *weakSelf = self;
+    //成功
     scanVC.BSScanSuccessBlock = ^(NSString *res){
         NSLog(@"BSScanSuccessBlock res=%@", res);
     };
+    //失败
     scanVC.BSScanFailBlock = ^(NSString *res){
         NSLog(@"BSScanFailBlock res=%@", res);
     };
+    //取消
     scanVC.BSScanCancelBlock = ^(NSString *res){
         NSLog(@"BSScanCancelBlock res=%@", res);
     };
